f2a413526138177de74eebf2930f1289b79f4ec8
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-07 16:51:47 +0300

    created a new file.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,7 +54,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
+def get_db() -> MySQLConnection:
     """Returns a connector to a MYSQL database."""
 
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
