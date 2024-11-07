e177505d72520aa0da9637f3c8dd3fafea58e156
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-07 13:21:12 +0300

    created a new file.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,25 @@ import re
 from typing import List
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialisation continuation."""
+
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Where i'll implement the new code."""
+
+        record.msg = filter_datum(
+                                  self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
 def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """Obfuscates field in a log message."""
