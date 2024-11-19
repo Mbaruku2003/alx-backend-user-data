ac54f952ccce1b450456f691c48fbf38439a1f7c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-19 11:13:24 +0300

    created a new file.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""takes in a password and returns bytes"""
+"""takes in a password and returns bytes."""
 import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
-    """define hashpasswod in bytes."""
+    """defined the hashed password returning bytes."""
 
-    salt = bcrypt.gensalt()
-    the_passwd = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return the_passwd
+    salted_password = bcrypt.gensalt()
+    encoded_password = bcrypt.hashpw(password.encoding('utf-8'), salted_password)
+    return encoded_password
