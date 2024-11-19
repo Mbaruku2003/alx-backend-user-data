f20b566b2f8f51439fcf3eb709de5222fab726b4
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-19 16:57:36 +0300

    created a new file.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Define an app using flask."""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -11,6 +14,20 @@ def welcome():
     """Get  a Json payload with a welcome message."""
 
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'])
+def users():
+    """POSTS uSERS."""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        return jsonify({"message": "email and password are required"}), 400
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
