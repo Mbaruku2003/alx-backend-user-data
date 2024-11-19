a50734a84f781529b6198e3f8d9168e044f4139f
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-19 13:05:28 +0300

    created a new file.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Define a way to know a user."""
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-import typing
-from typing import Column
+from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
@@ -13,9 +11,13 @@ class User(Base):
 
     __tablename__ = "users"
 
-    id = Column(Integer, AutoIncrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
 
+if __name__ == "__main__":
+    print(User.__tablename__)
+    for column in User.__table__.columns:
+        print("{}: {}".format(column, column.type))
