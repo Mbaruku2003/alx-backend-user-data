969da5130dbb347303cb2bcb1c4a57b2097f2191
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-19 15:17:16 +0300

    created a new file.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,19 +12,3 @@ def _hash_password(password: str) -> bytes:
     salted_password = bcrypt.gensalt()
     encoded_password = bcrypt.hashpw(password.encode('utf-8'), salted_password)
     return encoded_password
-
-class Auth:
-    """Auth class to interact with the authentication database."""
-    def __init__(self):
-        self._db = DB()
-
-    def register_user(email: str, password:str) -> User:
-        """registers a user with the email and password."""
-
-        try:
-            self._db.find_user_by(email=email)
-            raise ValueError(f"user {email} already exists")
-        except NoResultFound:
-            hashed_password = _hashed_password(password).decode('utf-8')
-            new_user = self._db.add_user(email=email, hashed_password=hashed_password)
-            return new_user
