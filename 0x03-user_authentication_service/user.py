4d9c22130f4f56c2cc3310b5646586fec10c8dee
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-19 13:24:40 +0300

    created a new file.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,10 +11,10 @@ Base: Type = declarative_base()
 class User(Base):  # type: ignore
     """define table user to be used."""
 
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
