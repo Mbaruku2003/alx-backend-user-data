abaf6d8ff9baef74d4c06354bdf1f02d804fe3b7
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-19 11:58:47 +0300

    created a new file.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,8 +13,8 @@ class User(Base):
     __tablename__: str == 'users'
 
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250))
+    hashed_password = Column(String(250))
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
