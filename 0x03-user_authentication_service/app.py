420fbd20a56d2ddf381290a89bfc9d25e79aea8e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-20 18:04:57 +0300

    created a new file.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,25 +15,6 @@ def welcome():
 
     return jsonify({"message": "Bienvenue"})
 
-@app.route('/users', methods=['POST'])
-def users( -> Response):
-    """POSTS uSERS."""
-
-    if request.method == "POST":
-        the_email = request.form.get("email")
-        email = the_email.strip()
-        passwords = request.form.get("password")
-        password = passwords.strip()
-        try:
-            AUTH.register_user(email, password)
-            message = jsonify({"email": email,
-                              "message": "user created"})
-            return message
-        except Exception:
-            return jsonify({"message": "email already registered"})
-    else:
-        abort(400)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
