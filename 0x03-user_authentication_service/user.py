c4b33db44de9b4a27c937eefd40005ce65dfd2e6
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-20 18:26:30 +0300

    created a new file.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from typing import Any, Type
 
 
-Base: Type = declarative_base()
+Base = declarative_base()
 
 
 class User(Base):  # type: ignore
